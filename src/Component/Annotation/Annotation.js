ff6614d1918123a9c4987d5601a1af5705701020
Aditya <aditya.kumar1@kelltontech.com>
2021-05-28 01:38:20 +0530

    init code

--- src/Component/Annotation/Annotation.js
+++ src/Component/Annotation/Annotation.js
@@ -6,21 +6,155 @@ class Annotation extends Component {
         super(props)
         this.state = {
             loader: false,
+            videoFile: null,
+            uploaded: false,
+            successMsg: '',
+            excelFile: null,
+            uploadedFile: false,
+            imgSize: null,
+            uploadedModel: false
 
         }
     }
+    selectImgSize = (e) => {
+        if (e.target.value !== '') {
+            this.setState({
+                imgSize: e.target.value
+            })
+        } else {
+            alert('Please Select Image Size')
+        }
+    }
+    uploadVideo = (e) => {
+        if (e.target.files[0]) {
+            console.log(e.target.files[0])
+            this.setState({
+                videoFile: e.target.files[0],
+                uploaded: true
+            })
+        }
+    }
+    uploadExcelFile = (e) => {
+        if (e.target.files[0]) {
+            console.log(e.target.files[0])
+            this.setState({
+                excelFile: e.target.files[0],
+                uploadedFile: true
+            })
+        }
+    }
+    uploadExcelFileData = () => {
+        window.localStorage.setItem("file", this.state.excelFile.name);
+        this.setState({
+            loader: true
+        })
+        let formdata = new FormData();
+        formdata.append("file", this.state.excelFile, this.state.excelFile.name);
+        // formdata.append("fileName", this.state.excelFile.name);
+        // formdata.append("img_size", this.state.imgSize);
+        // formdata.append("test_video", window.localStorage.getItem('video'));
 
+        let requestOptions = {
+            method: 'POST',
+            body: formdata,
+            redirect: 'follow'
+        };
+
+
+        fetch("http://3.7.113.14/upload_model", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result), this.setState({
+                loader: false,
+                excelFile: null,
+                successMsg: 'Uploaded Successfully',
+                uploadedModel: true,
+                uploaded: true
+            }))
+            .catch(error => console.log('error', error), this.setState({
+                excelFile: null,
+                successMsg: 'Something Went Wrong !',
+                uploaded: true
+            }));
+    }
+    uploadVideoFile = () => {
+        window.localStorage.setItem("video", this.state.videoFile.name);
+        this.setState({
+            loader: true
+        })
+        let formdat = new FormData();
+        // formdata.append("excelFile", this.state.videoFile, this.state.videoFile.name);
+        formdat.append("file", this.state.videoFile, this.state.videoFile.name);
+
+        let requestOption = {
+            method: 'POST',
+            body: formdat,
+            redirect: 'follow'
+        };
+        fetch("http://3.7.113.14/upload_video", requestOption)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result)
+                    this.setState({
+                        loader: false,
+                        uploaded: false,
+                        videoFile: null,
+                        successMsg: 'Uploaded Successfully'
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        uploaded: false,
+                        videoFile: null,
+                        successMsg: 'Something Went Wrong !'
+                    });
+                }
+            )
+
+    }
+    runModel = () => {
+        this.setState({
+            loader: true
+        })
+        let formdata = new FormData();
+        formdata.append("fileName", window.localStorage.getItem('file'));
+        formdata.append("img_size", this.state.imgSize);
+        formdata.append("test_video", window.localStorage.getItem('video'));
+
+        let requestOptions = {
+            method: 'POST',
+            body: formdata,
+            redirect: 'follow'
+        };
+
+
+        fetch("http://3.7.113.14/get_annotations", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result), this.setState({
+                loader: false,
+                uploadedModel: false,
+                imgSize: null
+            }))
+            this.props.history.push('/testing')
+            .catch(error => console.log('error', error), this.setState({
+                successMsg: 'Something Went Wrong !',
+            }));
+    }
     render() {
         // const { loader } = this.state
         return (
             <>
+                {this.state.loader &&
+                    <div className="loader_wrapper">
+                        <div className="loader"></div>
+                    </div>}
                 <div className="container-fluid">
                     <nav className="navbar navbar-light guise-navbar">
                         <a className="navbar-brand" href="#">
                             <img src={logo.default} alt="" />
                         </a>
                         <ul>
-                            <li>
+                            <li className="active">
                                 <img src={annotation.default} alt="" />
                                 Annotation
                             </li>
@@ -29,10 +163,10 @@ class Annotation extends Component {
                                 A/B Testing
                             </li>
                         </ul>
-                        <div className="right_btn_nav">
+                        {/* <div className="right_btn_nav">
                             <button>Reset</button>
                             <button className="guise_btn">Save & Next</button>
-                        </div>
+                        </div> */}
                     </nav>
                 </div>
                 <div className="main_container">
@@ -42,48 +176,83 @@ class Annotation extends Component {
                                 <div className="outer_wrap">
                                     <label htmlFor="test">
                                         <div className="upload_wrap">
-                                    <div className="upload_img">
-                                        <img src={upload.default} alt="" />
-                                    </div>
-                                    <h4>Upload a New Video</h4>
-                                    <input type="file" id="test"/>
-                                    <p>Click in the button or drag and drop your file here</p>
-                                    <button className="guise_btn btn_lg" >Upload a File</button>
-                                    <div className="warning_text">Max. File Size : 100MB</div>
-                                </div>
+                                            <div className="upload_img">
+                                                <img src={upload.default} alt="" />
+                                            </div>
+                                            <h4>Upload a New Video</h4>
+                                            <input type="file" id="test" onChange={(e) => this.uploadVideo(e)} accept=".mp4" name="vide_file" />
+                                            <p>Click in the button or drag and drop your file here</p>
+
+                                            <div className="warning_text">Max. File Size : 100MB</div>
+                                        </div>
                                     </label>
+                                    {this.state.uploaded && this.state.videoFile !== null &&
+                                        <button className="guise_btn btn_lg" onClick={this.uploadVideoFile}>Upload a File</button>
+                                    }
                                     <div className="doc_process">
                                         <ul>
-                                           <li> 
-                                                <img src={vid_icon.default}/>
-                                                   <div className="progress_bar">
-                                                       <div className="legends">
-                                                           <span className="filename">lorem Ipsum.mp4</span>
-                                                           <span className="completed">84%</span>
-                                                       </div>
-                                                        <span className="bar_place"><span className="bar" style={{width: '84%'}}></span></span>
-                                                   </div> 
-                                               <span className="close">&times;</span>
-                                            </li> 
+                                            <li>
+                                                <img src={vid_icon.default} />
+                                                <div className="progress_bar">
+                                                    <div className="legends">
+                                                        <span className="filename">{this.state.videoFile && this.state.videoFile.name}</span>
+                                                        <span className="completed">{this.state.uploaded ? '100%' : '0%'}</span>
+                                                    </div>
+                                                    <span className="bar_place"><span className="bar" style={this.state.uploaded ? { width: '100%' } : { width: '0%' }}></span></span>
+                                                </div>
+                                                <span className="close">&times;</span>
+                                            </li>
+                                            <li>
+                                                {!this.state.uploaded &&
+                                                <p className="text-success">{this.state.successMsg}</p>}
+                                            </li>
                                         </ul>
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-sm-6">
+                            <div className="col-sm-6 pr-0">
                                 <div className="right_wrap">
-                                    <h4 className="upload_head">
-                                        <img src={vid_upload.default}/>Upload Model
+                                    <div className="right_one">
+                                        <div>
+                                            <h4 className="upload_head">
+                                                <img src={vid_upload.default} />Upload Model
+                                            </h4>
+                                            <label htmlFor="test1">
+                                                <input className="custom-file-input" type="file" id="test1" onChange={(e) => this.uploadExcelFile(e)} accept=".pt" name="excelFile" 
+                                                selectedFile={this.state.uploadedFile && 'yes'}/>
+
+                                            </label>
+                                            {this.state.uploadedModel&&
+                                            <p className="text-success">{this.state.successMsg}</p>}
+                                            {this.state.uploadedFile ?
+                                                <button className="guise_btn uploadExcel_btn" onClick={this.uploadExcelFileData}>Upload</button> : null}
+                                        </div>
+                                    </div>
+                                    <div className="right_two">
+                                        <div>
+                                            <h4 className="upload_head">
+                                                <img src={img_placeholder.default} />Choose Image Size
                                     </h4>
-                                    <button className="guise_btn">Upload File</button>
-                                    <h4 className="upload_head">
-                                        <img src={img_placeholder.default}/>Choose Image Size
-                                    </h4>
-                                    <select className="form-control">
-                                        <option value="129">129</option>
-                                        <option value="129">129</option>
-                                        <option value="129">129</option>
-                                    </select>
+                                            {this.state.excelFile === null ?
+                                                <select className="form-control" disabled>
+                                                    <option defaultValue={null}>Choose Image Size</option>
+                                                    
+                                                </select> :
+                                                <select className="form-control" onChange={(e) => this.selectImgSize(e)}>
+                                                    <option defaultValue={null}>Choose Image Size</option>
+                                                    <option defaultValue="128">128</option>
+                                                    <option defaultValue="256">256</option>
+                                                    <option defaultValue="416">416</option>
+                                                    <option defaultValue="512">512</option>
+                                                    <option defaultValue="640">640</option>
+                                                    <option defaultValue="1280">1280</option>
+                                                </select>}
+                                        </div>
+
+                                    </div>
                                 </div>
+                                {this.state.uploadedModel &&
+                                <button className="guise_btn uploadExcel_btn text-right" onClick={this.runModel}>Run Model</button>}
                             </div>
                         </div>
                     </div>
--- src/Component/Testing/Testing.js
+++ src/Component/Testing/Testing.js
@@ -5,14 +5,46 @@ class Testing extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            index: 0
+            index: 0,
+            frameSize: 1
         }
 
     }
     handleSelect = (selectedIndex, e) => {
         this.setState({ index: selectedIndex })
     };
+    frameAnnotations = (frameSize) => {
+        this.setState({
+            loader: true
+        })
+        let formdata = new FormData();
+        formdata.append("model_name", window.localStorage.getItem('file'));
+        formdata.append("frame", frameSize);
+        formdata.append("video", window.localStorage.getItem('video'));
 
+        let requestOptions = {
+            method: 'POST',
+            body: formdata,
+            redirect: 'follow'
+        };
+
+
+        fetch("http://3.7.113.14/get_frames_Annotations", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result), this.setState({
+                loader: false,
+                totalFrameSize: frameSize
+            }))
+            .catch(error => console.log('error', error), this.setState({
+                successMsg: 'Something Went Wrong !',
+            }));
+    }
+    componentDidMount() {
+        this.frameAnnotations(this.state.frameSize)
+    }
+    backToHome = () => {
+        this.props.history.push('/')
+    }
     render() {
         // const { loader } = this.state
         return (
@@ -27,15 +59,12 @@ class Testing extends Component {
                                 <img src={annotation.default} alt="" />
                                 Annotation
                         </li>
-                            <li>
+                            <li className="active">
                                 <img src={testing.default} alt="" />
                                 A/B Testing
                         </li>
                         </ul>
-                        <div className="right_btn_nav">
-                            <button>Back</button>
-                            <button className="guise_btn">Annotated Data</button>
-                        </div>
+
                     </nav>
                 </div>
                 <div className="main_container">
@@ -51,65 +80,49 @@ class Testing extends Component {
                                                 alt="First slide"
                                             />
                                         </Carousel.Item>
-                                        <Carousel.Item>
-                                            <img
-                                                className="d-block w-100"
-                                                src={slider2.default}
-                                                alt="Second slide"
-                                            />
-                                        </Carousel.Item>
-                                        <Carousel.Item>
-                                            <img
-                                                className="d-block w-100"
-                                                src={slider2.default}
-                                                alt="Third slide"
-                                            />
-                                        </Carousel.Item>
                                     </Carousel>
-                                    <div className="slide_indicator">Frame {this.state.index}/15</div>
+                                    <div className="slide_indicator">Frame {this.state.frameSize}/{this.state.totalFrameSize}</div>
                                 </div>
                             </div>
-                            <div className="col-sm-3">
+                            <div className="col-sm-3 right_testing_col">
                                 <div className="vechile_list">
                                     <h4><span>Object</span> <span>Accuracy<img src={sort_icon.default} /></span></h4>
                                     <ul className="list_check">
                                         <li>
                                             <label htmlFor="car">
-                                                <input type="checkbox" id="car" /> Car
-    </label>
+                                                <input type="checkbox" id="car" /> Car</label>
                                             <span className="accuracy">0.89</span>
                                         </li>
                                         <li>
                                             <label htmlFor="car">
-                                                <input type="checkbox" id="car" /> Car
-    </label>
+                                                <input type="checkbox" id="car" /> Car </label>
                                             <span className="accuracy">0.89</span>
                                         </li>
                                         <li>
                                             <label htmlFor="car">
-                                                <input type="checkbox" id="car" /> Car
-    </label>
+                                                <input type="checkbox" id="car" /> Car </label>
                                             <span className="accuracy">0.89</span>
                                         </li>
                                         <li>
                                             <label htmlFor="car">
-                                                <input type="checkbox" id="car" /> Car
-    </label>
+                                                <input type="checkbox" id="car" /> Car </label>
                                             <span className="accuracy">0.89</span>
                                         </li>
                                         <li>
                                             <label htmlFor="car">
-                                                <input type="checkbox" id="car" /> Car
-    </label>
+                                                <input type="checkbox" id="car" /> Car</label>
                                             <span className="accuracy">0.89</span>
                                         </li>
                                         <li>
                                             <label htmlFor="truck">
-                                                <input type="checkbox" id="truck" /> Trcuk
-    </label>
+                                                <input type="checkbox" id="truck" /> Trcuk</label>
                                             <span className="accuracy">0.89</span>
                                         </li>
                                     </ul>
+                                    <div className="right_btn_nav text-center">
+                                        <button onClick={this.backToHome}>Back</button>
+                                        <button className="guise_btn">Annotated Data</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
